a4c319131913eb96a8e577aaa0bde267857992a6
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2025-08-08 12:15:03 -0300

    Conclusão do Projeto

--- app/jogo/[portas]/[temPresente]/page.tsx
+++ app/jogo/[portas]/[temPresente]/page.tsx
@@ -13,7 +13,19 @@ import { useRouter } from "next/router"
 export default function jogo() {
 
   const searchParams = useParams()
+  const [valido, setValido] = useState(false)
   const [portas, setPortas] = useState([])
+
+  useEffect(() => {
+    const portas = +searchParams.portas
+    const temPresente = +searchParams.temPresente
+    
+    const qtdePortasValidas = portas >= 3 && portas <= 30
+    const temPresenteValido = temPresente >= 1 && temPresente <= portas
+
+    setValido(qtdePortasValidas && temPresenteValido)
+
+  }, [portas])
 
   useEffect(() => {
     const portas = +searchParams.portas
@@ -21,11 +33,9 @@ export default function jogo() {
     setPortas(criarPortas(portas, temPresente))
   }, [searchParams])
 
-  // +router.query.portas
-  // +router.query.temPresente
 
   function renderizarPortas() {
-      return portas.map(porta => {
+      return valido && portas.map(porta => {
         return <Porta key={porta.getNumero()} value={porta} onChange={novaPorta => {
           setPortas(atualizarPortas(portas, novaPorta))
         }} />
@@ -35,7 +45,10 @@ export default function jogo() {
   return (
     <div className={styles.jogo}>
       <div className={styles.portas}>
-        {renderizarPortas()}
+        {valido ?
+        renderizarPortas() :
+        <h2>Valores inválidos</h2>
+        }
       </div>
       <div className={styles.botoes}>
         <Link href='/'>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+'use client'
+
 import styles from '../styles/Formulario.module.css'
 import Cartao from '../components/Cartao'
+import EntradaNumerica from '../components/EntradaNumerica'
 import Link from 'next/link';
+import { useState } from 'react';
 
 
 export default function Formulario() {
-  
+  const [qtdePortas, setQtdePortas] = useState(3) 
+  const [comPresente, setComPresente] = useState(1)
+
+
   return (
     <div className={styles.formulario}>
       <div>
@@ -12,14 +19,18 @@ export default function Formulario() {
           <h1>Monty Hall</h1>
         </Cartao>
 
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text='Qtde Portas?' value={qtdePortas} onChange={novaQtde => setQtdePortas(novaQtde)}/>
+        </Cartao>
       </div>
       <div>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text='Porta com Presente?' value={comPresente} onChange={novaPortaComPresente => setComPresente(novaPortaComPresente)}/>
+        </Cartao>
 
         <Cartao bgColor="#28a085">
-          <Link href={`/jogo/4/2`}>
-            <h2 className={styles.link}>Iniciar</h2>
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`} className={styles.link}>
+            <h2>Iniciar</h2>
           </Link>
         </Cartao>
       </div>
